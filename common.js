455b3cc91f7690e23b6944e40ad77ca18613bc6d
linx467 <linx76344@gmail.com>
2025-06-27 23:32:56 +0400

    Add files via upload

---
+++ common.js
@@ -0,0 +1,10 @@
+
+const input = document.getElementById("input");
+export const backspacebtn = document.getElementById("backspace")
+export const buttonsContainer = document.getElementById("buttons-container")
+
+
+
+export function DisplayInput(Result){
+    input.value = Result;
+}
